74cfbcbe274a311b963d01f0138ca24f1a010728
Christopher Fiel Jr <cjfiel322@gmail.com>
2025-09-29 09:23:21 +0800

    Added explore view functions

--- app/routes.py
+++ app/routes.py
@@ -160,6 +160,8 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
+    page = request.args.get('page', 1, type=int)
     query = sa.select(Post).order_by(Post.timestamp.desc())
-    posts = db.session.scalars(query).all()
-    return render_template('index.html', title='Explore', posts=posts)
+    posts = db.paginate(query, page=page,
+                        per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    return render_template("index.html", title='Explore', posts=posts.items)
